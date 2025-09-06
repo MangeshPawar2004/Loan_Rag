ade60c9d35639bb4e8e043c7f5fc8cbe1ffe65c2
MangeshPawar2004 <mangeshpawarmrp2004@gmail.com>
2025-09-06 22:15:55 +0530

    updated predictLoan

--- loan recommendation system/src/pages/PredictLoan.jsx
+++ loan recommendation system/src/pages/PredictLoan.jsx
@@ -49,7 +49,7 @@ export default function PredictLoan() {
     };
 
     try {
-      const response = await fetch("https://loan-rag-backend.onrender.com/predict", {
+      const response = await fetch("http://127.0.0.1:8000/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
