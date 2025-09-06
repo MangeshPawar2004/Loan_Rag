ed56f8ae5833bb901ab8197bf208ca5bab919555
MangeshPawar2004 <mangeshpawarmrp2004@gmail.com>
2025-09-06 22:05:21 +0530

    cors origins updated

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Loan Prediction API")
 origins = ["http://localhost:5173", "http://127.0.0.1:5173","https://loan-rag.vercel.app"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins or ["*"],
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
